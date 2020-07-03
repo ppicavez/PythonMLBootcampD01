6749167c17c2c50cfb08538089b371516509f546
ppicavez <ppicavez@student.42lyon.fr>
2020-07-03 17:38:54 +0200

    exo 04 done

--- ex03/gradient.py
+++ ex03/gradient.py
@@ -16,6 +16,14 @@ def simple_gradient(x, y, theta):
     Raises:
     This function should not raise any Exception.
     """
+
+    if not len(x) or not len(y) or not len(theta):
+        return None
+    if len(x.shape) != 1 or len(y.shape) != 1 or theta.shape != (2, ):
+        return None
+    if len(x) != len(y):
+        return None
+
     y_hat = np.dot(np.c_[np.ones((len(x), 1)), x], theta)
     result = []
     result.append(theta[0] - (sum(y_hat - y) / len(x)))
@@ -23,9 +31,10 @@ def simple_gradient(x, y, theta):
     return np.array(result)
 
 
-x = np.array([12.4956442, 21.5007972, 31.5527382, 48.9145838, 57.5088733])
-y = np.array([37.4013816, 36.1473236, 45.7655287, 46.6793434, 59.5585554])
-theta1 = np.array([2, 0.7])
-print(simple_gradient(x, y, theta1))
-theta2 = np.array([1, -0.4])
-print(simple_gradient(x, y, theta2))
+if __name__ == "__main__":
+    x = np.array([12.4956442, 21.5007972, 31.5527382, 48.9145838, 57.5088733])
+    y = np.array([37.4013816, 36.1473236, 45.7655287, 46.6793434, 59.5585554])
+    theta1 = np.array([2, 0.7])
+    print(simple_gradient(x, y, theta1))
+    theta2 = np.array([1, -0.4])
+    print(simple_gradient(x, y, theta2))
