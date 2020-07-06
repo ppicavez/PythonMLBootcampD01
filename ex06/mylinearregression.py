8c141873307202692698767d07914ac4059a9eb1
ppicavez <ppicavez@student.42lyon.fr>
2020-07-06 17:17:10 +0200

    exo 06 done

---
+++ ex06/mylinearregression.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+class MyLinearRegression:
+    """
+    Description:
+    My personnal linear regression class to fit like a boss.
+    """
+    def __init__(self, thetas, alpha=0.001, max_iter=1000):
+        self.alpha = alpha
+        self.max_iter = max_iter
+        self.thetas = thetas
+
+    def predict_(self, x):
+        return np.dot(np.c_[np.ones((len(x), 1)), x], self.thetas)
+
+    def fit_(self, x, y):
+        X = np.c_[np.ones((len(x), 1)), x]
+        y_hat = self.predict_(x)
+        y = np.squeeze(y)
+        i = 0
+        while i < self.max_iter:
+            X_prime = np.c_[np.ones((len(x), 1)), x]
+            gradient = (X_prime.T @ (X_prime @ self.theta - y)) / len(x)
+            self.thetas = self.thetas - self.alpha * gradient
+            i += 1
+
+        return self.thetas
+
+    def cost_elem_(self, x, y):
+        m = len(x)
+        y_hat = self.predict_(x)
+        print(y_hat)
+        y = np.squeeze(y)
+        return (((y_hat - y) ** 2) / (2 * m))
+
+    def cost_(self, x, y):
+        elem = self.cost_elem_(x, y)
+        return sum(elem)
+
+
---
+++ ex10/test.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from mylinearregression import MyLinearRegression as MyLR
+
+
+data = pd.read_csv("are_blue_pills_magics.csv")
+Xpill = np.array(data['Micrograms']).reshape(-1,1)
+Yscore = np.array(data['Score']).reshape(-1,1)
+linear_model1 = MyLR(np.array([[89.0], [-8]]))
+linear_model2 = MyLR(np.array([[89.0], [-6]]))
+Y_model1 = linear_model1.predict_(Xpill)
+Y_model2 = linear_model2.predict_(Xpill)
+print(linear_model1.mse_(Xpill, Yscore))
+print(mean_squared_error(Yscore, Y_model1))
+print(linear_model2.mse_(Xpill, Yscore))
+print(mean_squared_error(Yscore, Y_model1))
